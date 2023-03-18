3a501b62f8b117a8d4e871cbca93de97c7e6db5f
jemmy <flashyjotebele@gmail.com>
2023-03-18 14:48:33 +0100

    lang splash screen done with new design

--- src/components/SplashScreenComponents/SplashScreenBg.jsx
+++ src/components/SplashScreenComponents/SplashScreenBg.jsx
@@ -2,11 +2,11 @@ import logo from "../../assets/logo.svg";
 
 function SplashScreenBg({ children }) {
   return (
-    <section className="bg-bgBlack text-yellow-500 h-full flex flex-col justify-between items-center pt-10 bg-club-bg-splash-screen bg-center bg-no-repeat">
+    <section className="bg-bgBlack text-yellow-500 min-h-full flex flex-col justify-between items-center pt-10 bg-club-bg-splash-screen bg-center bg-no-repeat">
       <figure>
         <img src={logo} alt="logo" width={170} height={50} />
       </figure>
-      <div className="splash-bg-blur absolute top-2 -right-24 "></div>
+      {/* <div className="splash-bg-blur translate-x-28"></div> */}
       {children}
     </section>
   );
