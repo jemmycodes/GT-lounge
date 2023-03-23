ae390935a6e1f3cd0914874f9fb38f8a21038569
jemmy <flashyjotebele@gmail.com>
2023-03-23 16:27:37 +0100

    stopped overflow coming from sliding human illustrations in splash screen

--- src/components/SplashScreenComponents/SplashScreenBg.jsx
+++ src/components/SplashScreenComponents/SplashScreenBg.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/logo.svg";
 
 function SplashScreenBg({ children }) {
   return (
-    <section className="bg-bgBlack text-yellow-500 min-h-full overflow-hidden flex flex-col justify-between items-center pt-10 bg-club-bg-splash-screen bg-center bg-no-repeat">
+    <section className="bg-bgBlack text-yellow-500 h-full overflow-hidden flex flex-col justify-between items-center pt-10 bg-club-bg-splash-screen bg-center bg-no-repeat">
       <figure className=" scale-100 delay-1000">
         <img src={logo} alt="logo" width={170} height={50} />
       </figure>
