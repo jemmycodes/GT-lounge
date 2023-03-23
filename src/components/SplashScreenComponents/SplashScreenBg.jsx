a3ab8524718ef4de98d1e30ba68c84b11ad5dbd8
jemmy <flashyjotebele@gmail.com>
2023-03-24 00:03:57 +0100

    fixed-overlay-issues

--- src/components/SplashScreenComponents/SplashScreenBg.jsx
+++ src/components/SplashScreenComponents/SplashScreenBg.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.svg";
 function SplashScreenBg({ children }) {
   return (
     <section className="bg-bgBlack text-yellow-500 h-full overflow-hidden flex flex-col justify-between items-center pt-10 bg-club-bg-splash-screen bg-center bg-no-repeat">
-      <div className="splash-bg-blur absolute top-4 right-0 overflow-hidden  top-4 translate-x-24"></div>
+      <div className="splash-bg-blur absolute top-4 -right-24 overflow-hidden  "></div>
       {children}
     </section>
   );
