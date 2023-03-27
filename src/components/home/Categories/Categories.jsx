e8c9038ea762c8407c554206d42e613e35fb01eb
jemmy <flashyjotebele@gmail.com>
2023-03-27 23:44:28 +0100

    categories section done

--- src/components/home/Categories/Categories.jsx
+++ src/components/home/Categories/Categories.jsx
@@ -3,7 +3,7 @@ import { Category } from "../../index";
 
 const Categories = () => {
   return (
-    <ul className="flex gap-5  overflow-x-scroll bg-red-400 py-5 scrollbar-hide">
+    <ul className="flex gap-5  overflow-x-scroll ">
       {menuData.map(({ type, icon }) => (
         <Category key={type} icon={icon} type={type} />
       ))}
