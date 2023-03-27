057bb8bc560682e01f77561d30b3a644c8a9cf4e
jemmy <flashyjotebele@gmail.com>
2023-03-27 23:37:14 +0100

    fixing scrolling issue

--- src/components/home/Categories/Categories.jsx
+++ src/components/home/Categories/Categories.jsx
@@ -3,7 +3,7 @@ import { Category } from "../../index";
 
 const Categories = () => {
   return (
-    <ul className="flex gap-5  overflow-x-scroll scrollbar-hide">
+    <ul className="flex gap-5  overflow-x-scroll bg-red-400 py-5 scrollbar-hide">
       {menuData.map(({ type, icon }) => (
         <Category key={type} icon={icon} type={type} />
       ))}
