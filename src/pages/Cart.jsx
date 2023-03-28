07245fe45dfd2a48977316ab29b1a27117922333
jemmy <flashyjotebele@gmail.com>
2023-03-28 14:41:16 +0100

    fixed the cart scroll issue

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,20 +10,20 @@ const Cart = () => {
         <BsCart4 className="text-2xl font-bold" />
       </header>
       <main className="bg-bgBlack h-[90vh]  flex justify-between flex-col gap-5 rounded-t-3xl p-6 pb-1">
-        <section className="flex flex-col gap-6 h-[90%] overflow-y-auto scrollbar-hide">
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
+        <section className="flex flex-col gap-6 h-[80%] overflow-y-auto scrollbar-hide">
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
               <figure>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
                   alt=""
-                  height={40}
-                  width={80}
+                  height={30}
+                  width={70}
                   className="rounded-full aspect-square"
                 />
               </figure>
               <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
                 <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
               </span>
             </div>
@@ -35,19 +35,19 @@ const Cart = () => {
               <p className="cursor-pointer">-</p>
             </span>
           </hgroup>{" "}
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
               <figure>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
                   alt=""
-                  height={40}
-                  width={80}
+                  height={30}
+                  width={70}
                   className="rounded-full aspect-square"
                 />
               </figure>
               <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
                 <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
               </span>
             </div>
@@ -59,19 +59,19 @@ const Cart = () => {
               <p className="cursor-pointer">-</p>
             </span>
           </hgroup>{" "}
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
               <figure>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
                   alt=""
-                  height={40}
-                  width={80}
+                  height={30}
+                  width={70}
                   className="rounded-full aspect-square"
                 />
               </figure>
               <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
                 <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
               </span>
             </div>
@@ -83,43 +83,19 @@ const Cart = () => {
               <p className="cursor-pointer">-</p>
             </span>
           </hgroup>{" "}
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
               <figure>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
                   alt=""
-                  height={40}
-                  width={80}
+                  height={30}
+                  width={70}
                   className="rounded-full aspect-square"
                 />
               </figure>
               <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
-                <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
-              </span>
-            </div>
-            <span className="flex gap-3 items-center ">
-              <p className="cursor-pointer">+</p>
-              <p className="bg-black/50 px-2 py-[.05rem] rounded-lg cursor-pointer">
-                5
-              </p>
-              <p className="cursor-pointer">-</p>
-            </span>
-          </hgroup>
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
-              <figure>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
-                  alt=""
-                  height={40}
-                  width={80}
-                  className="rounded-full aspect-square"
-                />
-              </figure>
-              <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
                 <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
               </span>
             </div>
@@ -131,19 +107,91 @@ const Cart = () => {
               <p className="cursor-pointer">-</p>
             </span>
           </hgroup>{" "}
-          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 rounded-xl">
-            <div className="flex gap-7 items-center">
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
               <figure>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
                   alt=""
-                  height={40}
-                  width={80}
+                  height={30}
+                  width={70}
                   className="rounded-full aspect-square"
                 />
               </figure>
               <span className="flex gap-2 flex-col">
-                <h3 className="text-lg font-semibold">Ice Cream</h3>
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
+                <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
+              </span>
+            </div>
+            <span className="flex gap-3 items-center ">
+              <p className="cursor-pointer">+</p>
+              <p className="bg-black/50 px-2 py-[.05rem] rounded-lg cursor-pointer">
+                5
+              </p>
+              <p className="cursor-pointer">-</p>
+            </span>
+          </hgroup>{" "}
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
+              <figure>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
+                  alt=""
+                  height={30}
+                  width={70}
+                  className="rounded-full aspect-square"
+                />
+              </figure>
+              <span className="flex gap-2 flex-col">
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
+                <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
+              </span>
+            </div>
+            <span className="flex gap-3 items-center ">
+              <p className="cursor-pointer">+</p>
+              <p className="bg-black/50 px-2 py-[.05rem] rounded-lg cursor-pointer">
+                5
+              </p>
+              <p className="cursor-pointer">-</p>
+            </span>
+          </hgroup>{" "}
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
+              <figure>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
+                  alt=""
+                  height={30}
+                  width={70}
+                  className="rounded-full aspect-square"
+                />
+              </figure>
+              <span className="flex gap-2 flex-col">
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
+                <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
+              </span>
+            </div>
+            <span className="flex gap-3 items-center ">
+              <p className="cursor-pointer">+</p>
+              <p className="bg-black/50 px-2 py-[.05rem] rounded-lg cursor-pointer">
+                5
+              </p>
+              <p className="cursor-pointer">-</p>
+            </span>
+          </hgroup>{" "}
+          <hgroup className="bg-lang-card-color justify-between flex items-center text-gold p-4 gap-2 rounded-xl">
+            <div className="flex gap-2 items-center">
+              <figure>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/f/ff/2018-03-04_17_55_48_Large_serving_of_chocolate_soft-serve_ice_cream_at_the_Carvel_off_of_Denow_Road_in_Hopewell_Township%2C_Mercer_County%2C_New_Jersey.jpg"
+                  alt=""
+                  height={30}
+                  width={70}
+                  className="rounded-full aspect-square"
+                />
+              </figure>
+              <span className="flex gap-2 flex-col">
+                <h3 className="font-semibold whitespace-pre-wrap">Ice Cream</h3>
                 <h2 className="text-xl font-bold first-letter:text-sm">$12</h2>
               </span>
             </div>
